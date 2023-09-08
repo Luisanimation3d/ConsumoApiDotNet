3e1a78deb3ee0af2160882b5623745045a859618
SENA <SENA@MEDAPRCSGFSP549>
2023-09-08 11:40:22 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using WebApplication1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class CharacterController : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public CharacterController()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var apiUrl = "https://rickandmortyapi.com/api/character";
+            var response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var characters = JsonConvert.DeserializeObject<ApiResponse<Character>>(content);
+                return View(characters.Results);
+            }
+
+            return View(new List<Character>());
+        }
+
+        public class ApiResponse<T>
+        {
+            public Info Info { get; set; }
+            public List<T> Results { get; set; }
+        }
+
+        public class Info
+        {
+            public int Count { get; set; }
+            public int pages { get; set; }
+            public string Next { get; set; }
+            public string Prev { get; set; }
+        }
+    }
+}
---
+++ WebApplication1/Models/Character.cs
@@ -0,0 +1,28 @@
+﻿namespace WebApplication1.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Gender { get; set; }
+        public Origin Origin { get; set; }
+        public Location Location { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Origin
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+}
